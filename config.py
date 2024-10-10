4c80a622702cd6a77d3a99cde64f68f27cd35a9c
Vegapunk-shaka <slac777f@tuta.io>
2024-10-10 09:00:18 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 
 class Config(object):
     API_HASH = "03f1c29714a7ce9ed1819318427ea281"
-    BOT_TOKEN = "6487314038:AAHyEnNRnf_imx_TmwaejYtu4rUIydayuEI"
+    BOT_TOKEN = "6487314038:AAH28GvYkBWURxxZBqS1QOo-yBAvNIFh2o4"
     TELEGRAM_API = "20524455"
     OWNER = "1908235162"
     OWNER_USERNAME = "@ungaa_bungaa"
     PASSWORD = "lolicon"
     DATABASE_URL = "mongodb+srv://sanjitha:sanjitha01@cluster0.psghgex.mongodb.net/?retryWrites=true&w=majority"
-    LOGCHANNEL = "-1001802445568"   # Add channel id as: "-100 + Actual_ID"
+    LOGCHANNEL = "-1001930986503"   # Add channel id as: "-100 + Actual_ID"
 #USER_SESSION_STRING = "" # Premium account session string to upload upto 4GB (requires "LOGCHANNEL")
 
 # tired of redeploying :(
